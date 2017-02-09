678c3eef219000cbcc672e5c50a7105079445a05
Vasiliy Kaygorodov <vkaygorodov@gmail.com>
2017-02-09 17:52:12 +0100

    Implement "notify" configuration

--- cloud-init.py
+++ cloud-init.py
@@ -3,21 +3,46 @@
 from errbot import BotPlugin, webhook, re_botcmd
 from bottle import abort
 
+CONFIG_TEMPLATE = {'notify': []}
+
 class CloudInit(BotPlugin):
     """
     Err plugin to send messages to chats/users through webhooks
     """
 
-    def activate(self):
+     def configure(self, configuration):
+        """
+        Creates a Python dictionary object which contains all the values from our
+        CONFIG_TEMPLATE and then updates that dictionary with the configuration
+        received when calling the "!plugin config" command.
+        """
 
-        super(CloudInit, self).activate()
-        if not 'notify' in self.keys():
-            self.log.info("Initializing 'notify' storage")
-            self['notify'] = []
+        if configuration is not None and configuration != {}:
+            config = dict(chain(CONFIG_TEMPLATE.items(),
+                                configuration.items()))
+        else:
+            config = CONFIG_TEMPLATE
+        super(CloudInit, self).configure(config)
+
+    def get_configuration_template(self):
+        """
+        Defines the configuration structure this plugin supports
+        """
+        return CONFIG_TEMPLATE
 
     @webhook('/cloud_init/<instance_id>',raw=True)
     def cloud_init(self, request, instance_id):
         self.log.info("Instace {} is calling home...".format(instance_id))
-        for e in request.forms:
-            self.log.debug(repr(e))
-        self.log.debug(request.body)
+        host = request.forms.get('hostname', instance_id)
+        message = "Yay, {} reports itself ready!".format(host)
+        notify = self.config['notify']
+        if len(notify) > 0:
+            for dest in notify:
+                self.build_identifier(dest)
+                self.send(dest, message)
+                return None # HTTP 200
+        else:
+            self.warn_admins(message)
+            return None # HTTP 200
+        abort('500', 'Internal Server Error')
+
